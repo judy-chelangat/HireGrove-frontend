5fb1304c7d9a50e03daa39619dd046959fa342e3
judy-chelangat <judysigilai75@gmail.com>
2023-10-02 19:54:05 +0300

    added landing page

---
+++ src/LandingPage.jsx
@@ -0,0 +1,35 @@
+import React ,{ useState } from 'react';
+// import SignUpForm from './SignUpForm'
+import Navbar from './Navbar';
+
+function LandingPage() {
+    // const [showSignupForm,setShowSignupForm]=useState(false);
+
+    // const handleSignupClick =()=>{
+    //     setShowSignupForm(true)
+    // }
+  return (
+    <>
+        <Navbar/>
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col-lg-6 col-md-12 d-flex align-items-center">
+            <div>
+              <h1>Find freelance and full-time developer jobs</h1>
+              <p>HireGroove is your one-stop-center for thousands of digital freelance and full-time jobs</p>
+            </div>
+          </div>
+          <div className="col-lg-6 col-md-12">
+            <img
+              src='./public/image 138.png'
+              alt="a person"
+              className="img-fluid"
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default LandingPage
