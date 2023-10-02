12b1c6813c79d6273e96e2f674d19961fac0b734
judy-chelangat <judysigilai75@gmail.com>
2023-10-02 22:49:12 +0300

    created a route for the joblistings

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter , Route,Routes } from 'react-router-dom';
 import LandingPage from './LandingPage';
+import JobListings from './JobListings';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
     <BrowserRouter>
         <Routes>
             <Route exact path='/' element={<LandingPage/>}/>
+            <Route exact path='/jobs' element={<JobListings/>}/>
           
         </Routes>
     </BrowserRouter>
