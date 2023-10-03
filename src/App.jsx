1b59e932b4c6603fca0be27f553ee05aaba5a413
judy-chelangat <judysigilai75@gmail.com>
2023-10-03 17:36:21 +0300

    added route path for the job applied

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter , Route,Routes } from 'react-router-dom';
 import LandingPage from './LandingPage';
 import JobListings from './JobListings';
+import AppliedJobs from './AppliedJobs';
 
 function App() {
   return (
@@ -10,6 +11,8 @@ function App() {
         <Routes>
             <Route exact path='/' element={<LandingPage/>}/>
             <Route exact path='/jobs' element={<JobListings/>}/>
+            <Route path="/jobs/appliedjobs" element={<AppliedJobs />} />
+            {/* <Route path="/postjob" element={<PostJob />} /> */}
           
         </Routes>
     </BrowserRouter>
