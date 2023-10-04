5555f45e696301ed1140d0497bc060c577a08d45
judy-chelangat <judysigilai75@gmail.com>
2023-10-04 21:32:47 +0300

    added signup form

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import Signup from './SignUpForm';
 
 function Navbar() {
-  
+  const [isPopupVisible, setPopupVisible] = useState(false); // State for controlling popup visibility
+
+  const openPopup = () => {
+    setPopupVisible(true);
+  };
+
+  const closePopup = () => {
+    setPopupVisible(false);
+  };
+
   return (
     <div className="navbar">
-    
         <h3>HireGrove</h3>
         <div className="nav-links">
-         <Link to="./signup">SignUp</Link>
-         <Link to="./login">Login</Link>
-         </div>
-    
-    
-    </div>
+          <Link to="signup" onClick={openPopup}>SignUp</Link>
+          <Link to="./login">Login</Link>
+        </div>
+        {isPopupVisible && (
+          <Signup closePopup={closePopup} /> // Pass closePopup function as a prop to Signup
+        )}
+      </div>
   );
 }
 
