27989fa6f24b35ebe23e6c018954e9f373177912
judy-chelangat <judysigilai75@gmail.com>
2023-10-04 13:51:13 +0300

    added use effect

--- src/JobListings.jsx
+++ src/JobListings.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState ,useEffect} from 'react';
 import JobListing from './JobListing';
 import { Link } from 'react-router-dom';
 
@@ -6,33 +6,33 @@ import { Link } from 'react-router-dom';
 function JobListings() {
   const [searchTerm, setSearchTerm] = useState(""); // State to hold the user's input
   const [locationFilter, setLocationFilter] = useState(""); // State to hold the location filter
-    // // fetching the jobs from the backend api
-    // const [jobListings, setJobListings] = useState([]);
+    // fetching the jobs from the backend api
+  const [jobListings, setJobListings] = useState([]);
 
-    // useEffect(() => {
-    //     // Fetch the job listings from the backend API
-    //     fetch("/api/job-listings")
-    //       .then((response) => response.json())
-    //       .then((jobListings) => setJobListings(jobListings));
-    //   }, []);
-    const jobListings = [
-        {
-          id: 1,
-          title: "Software Engineer",
-          description: "We are looking for a talented software engineer to join our team. You will be responsible for developing and maintaining our web applications.",
-          company_name: "Google",
-          location: "Mountain View, CA",
-          posted_at: "2023-08-04"
-        },
-        {
-          id: 2,
-          title: "Product Manager",
-          description: "We are looking for a product manager to help us develop and launch new products. You will be responsible for gathering user requirements, working with engineers to design and develop products, and launching and marketing new products.",
-          company_name: "Facebook",
-          location: "Menlo Park, CA",
-          posted_at: "2023-08-03"
-        }
-      ];
+    useEffect(() => {
+        // Fetch the job listings from the backend API
+        fetch("https://job-application-backend.onrender.com/Available jobs")
+          .then((response) => response.json())
+          .then((jobListings) => setJobListings(jobListings));
+      }, []);
+    // const jobListings = [
+    //     {
+    //       id: 1,
+    //       title: "Software Engineer",
+    //       description: "We are looking for a talented software engineer to join our team. You will be responsible for developing and maintaining our web applications.",
+    //       company_name: "Google",
+    //       location: "Mountain View, CA",
+    //       posted_at: "2023-08-04"
+    //     },
+    //     {
+    //       id: 2,
+    //       title: "Product Manager",
+    //       description: "We are looking for a product manager to help us develop and launch new products. You will be responsible for gathering user requirements, working with engineers to design and develop products, and launching and marketing new products.",
+    //       company_name: "Facebook",
+    //       location: "Menlo Park, CA",
+    //       posted_at: "2023-08-03"
+    //     }
+    //   ];
       // Filtering the jobs based on the search term
       const filteredJobs = jobListings.filter((job) => {
         const jobTitle = job.title || "";
